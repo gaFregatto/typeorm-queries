dd5fac31a17c2d8740b62aff09fca0d8b85bd866
Guilherme Arai Fregatto <guilherme_arai19@hotmail.com>
2021-03-30 16:17:39 -0300

    complete

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,24 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    // Complete usando query builder
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("games.title ILIKE :title", {title: `%${param}%`})
+      .getMany()
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    // Complete usando raw query
+    return await this.repository.query(`SELECT COUNT(*) FROM games`); 
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    // Complete usando query builder
+    return await this.repository
+    .createQueryBuilder()
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
   }
 }
